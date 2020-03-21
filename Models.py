7bc100504cf2c9e15e72a123636fec5ab57034ff
Alexis DAMIENS <winter.ensiie@gmail.com>
2020-03-21 15:17:23 +0100

    Added Models template

---
+++ Models.py
@@ -0,0 +1,19 @@
+"""This module implements Models used to play Belote game."""
+
+
+class Player:
+    """This class implements players."""
+
+    pass
+
+
+class Card:
+    """This class implements cards."""
+
+    pass
+
+
+class Game:
+    """This class implements a game of belote."""
+
+    pass
